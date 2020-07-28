7285687b53b3e062b0b703e2a6de7d586b42f046
Yerlan Nauryzbayev <nauryzbayev.yerlan@gmail.com>
2020-07-28 09:35:38 +0600

    test dockerfile remove user django

--- nomad_auto_advert/microservices/urls.py
+++ nomad_auto_advert/microservices/urls.py
@@ -8,8 +8,10 @@ router = routers.DefaultRouter()
 router.register(r'', ServicesViewSet, base_name='microservices')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('health-check/', health_check_view, name="health-check"),
     path('car/update/', UpdateCarView.as_view()),
     path('car/delete/<int:car_ext>/', DeleteCarView.as_view()),
+    path('', include(router.urls)),
 ]
+
+
