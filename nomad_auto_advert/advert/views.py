291cec6c42053b22577cddc6e01f2db02c7f1dd0
Yerlan Nauryzbayev <nauryzbayev.yerlan@gmail.com>
2020-08-25 11:03:42 +0600

    comment create update

--- nomad_auto_advert/advert/views.py
+++ nomad_auto_advert/advert/views.py
@@ -162,6 +162,17 @@ class MyAdvertCommentViewSet(MultiSerializerViewSetMixin,
     def perform_create(self, serializer):
         serializer.save(profile=self.request.user)
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        data = serializer.data
+        data['profile'] = self.request.user.first_name + " " + self.request.user.last_name
+
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class AdvertCommentView(generics.ListAPIView):
     serializer_class = AdvertCommentSerializer
